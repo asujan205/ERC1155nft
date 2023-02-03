cf916ec4d21eb08aad0ddf5181448f98fee4cc89
Sujan Adhikari <Asujan205@gmail.com>
2023-02-03 12:50:43 +0545

    smartContractWriting

--- contracts/nft155.sol
+++ contracts/nft155.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts@4.8.1/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts@4.8.1/access/Ownable.sol";
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyMarketPlace is ERC1155,Ownable{
+constructor ()ERC1155("") {}
+using Counters for Counters.Counter;
+Counters.Counter private _tokenIds;
+Counters.Counter private _isSold;
+
+function _setURI(string memory newuri) public  onlyOwner{
+    _setURI(newuri);
+}
+
     
 }
