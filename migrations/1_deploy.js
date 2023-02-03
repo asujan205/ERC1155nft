8d1a4638973563bf36f82493bbd5b1e84bc0990e
Sujan Adhikari <Asujan205@gmail.com>
2023-02-03 22:44:53 +0545

    next-auth

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,5 +1,5 @@
 const Nfts = artifacts.require("MyMarketPlace");
 
 module.exports = async function (deployer) {
-  deployer.deploy(Nfts);
+  deployer.deploy(Nfts, "0x2d2071e0f10EacF7b1E17232300b9a9085b86366");
 };
