69118571550765bb4218c5c3a317e2c2d86b38cc
Sujan Adhikari <Asujan205@gmail.com>
2023-02-03 12:11:07 +0545

    truffle init

---
+++ contracts/nft155.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.8.1/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts@4.8.1/access/Ownable.sol";
+contract MyMarketPlace is ERC1155,Ownable{
+    
+}
